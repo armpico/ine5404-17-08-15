6e3612cc6ed3da77683f9a3f210781f895c824bc
pixels <pixels@150.162.147.84>
2017-08-15 09:57:06 -0300

    Model 2D e 3D criados

---
+++ src/main/java/ine5404/arthurmp/model/Cartesian3D.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5404.arthurmp.model;
+
+/**
+ *
+ * @author pixels
+ */
+public class Cartesian3D {
+    private Cartesian2D xy;
+    private double z;
+
+    public Cartesian3D(double x, double y, double z) {
+        xy = new Cartesian2D(x, y);
+        this.z = z;
+    }
+
+    public double getX() {
+        return xy.getX();
+    }
+
+    public void setX(double x) {
+        xy.setX(x);
+    }
+
+    public double getY() {
+        return xy.getY();
+    }
+
+    public void setY(double y) {
+        xy.setY(y);
+    }
+    
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "x=" + xy.getX() + ", y=" + xy.getY() + ", z=" + z;
+    }
+}
